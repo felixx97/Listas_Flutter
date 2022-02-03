9d0542180476399feb24f304c64cccdc02d36b19
felixx97 <abrahammfelixx@gmail.com>
2022-02-03 14:26:42 -0300

    remove and add

--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -27,14 +27,20 @@ class _TodoListPageState extends State<TodoListPage> {
                 controller: _textEditingController,
               ),
               Container(
-                height: 400,
-                child: ListView.builder(
+                height: 500,
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => Divider(),
                   //builder fornece o index
                   itemCount: Tarefas
                       .length, //a partir da quantidade que tem dentro da lista
                   itemBuilder: (context, index) {
                     return ListTile(
                       title: Text(Tarefas[index]),
+                      onLongPress: () {
+                        setState(() {
+                          Tarefas.removeAt(index);
+                        });
+                      },
                     );
                   },
                 ),
@@ -43,17 +49,34 @@ class _TodoListPageState extends State<TodoListPage> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.orange,
-        onPressed: () {
-          if (_textEditingController.text.length > 0) {
-            setState(() {
-              Tarefas.add(_textEditingController.text);
-            });
-            _textEditingController.clear(); //Limpar apos escrever
-          }
-        },
-        child: Icon(Icons.add),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment:
+            MainAxisAlignment.end, //Alinhando os floatingbottom na direita
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.orange,
+            onPressed: () {
+              if (_textEditingController.text.length > 0) {
+                setState(() {
+                  Tarefas.add(_textEditingController.text);
+                });
+                _textEditingController.clear(); //Limpar apos escrever
+              }
+            },
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.orange,
+            onPressed: () {
+              setState(() {
+                Tarefas = [];
+              });
+              _textEditingController.clear(); //Limpar apos escrever
+            },
+            child: Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
